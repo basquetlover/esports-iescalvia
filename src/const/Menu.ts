b248574c1ff91f4f8bbd33de5563ea1661bfde30
basquetlover <85557755+basquetlover@users.noreply.github.com>
2026-06-25 02:13:53 +0200

    Crear base proyecto

---
+++ src/const/Menu.ts
@@ -0,0 +1,32 @@
+export const MENU = [
+    {
+        nombre: 'Inici',
+        enlace: `/`,
+        estado: 'Activa'
+    },
+    {
+        nombre: 'Tornejos',
+        enlace: `/tornejos`,
+        estado: 'Pròximament'
+    },
+    {
+        nombre: 'Calendari',
+        enlace: `/calenadri`,
+        estado: 'Pròximament'
+    },
+    {
+        nombre: 'Notícies',
+        enlace: `/noticies`,
+        estado: 'Pròximament'
+    },
+    {
+        nombre: 'Històric',
+        enlace: `/historic`,
+        estado: 'Pròximament'
+    },
+    {
+        nombre: 'Galeria',
+        enlace: `/galeria`,
+        estado: 'Pròximament'
+    }
+]
